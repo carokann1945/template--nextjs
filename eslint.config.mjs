830635b88941b9fe35c78ce388378bea1df5ec75
carokann1945 <carokann1945@gmail.com>
2026-02-10 05:29:38 +0900

    [chore] initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierRecommended from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,7 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  { extends: [prettierRecommended] },
 ]);
 
 export default eslintConfig;
